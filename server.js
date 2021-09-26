eaef8c1324c5ddd1747661b3402fd18dabe285b0
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-26 00:05:54 -0400

    Adds a new pokemon to the index -does not render correctly

--- server.js
+++ server.js
@@ -2,11 +2,31 @@ const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon');
 
+const methodOverride = require('method-override')
+app.use(express.urlencoded({ extended: false}))
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 // INDEX
 app.get('/', (req, res) => {
 res.render('index.ejs', { data: pokemon });
 });
+
+//NEW
+app.get('/new', (req, res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/', (req, res)=>{    
+    pokemon.push(req.body)
+    res.redirect('/')
+})
+
+//DESTROY
+// app.delete('/:indexOfPokemonArray', (req, res) =>{
+//     pokemon.splice(req.params.indexOfPokemonArray,1)
+//     res.redirect('/')
+// })
 
 
 //SHOW 
