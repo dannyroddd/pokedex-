c54fbf62947b294501c23c155007f7c2d0d2258a
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-26 18:36:43 -0400

    Adds a feature to edit pokemon names but doesnt give them a new show page/ doesnt render how i  wanted it to.

--- server.js
+++ server.js
@@ -8,29 +8,43 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 
 // INDEX
-app.get('/', (req, res) => {
+app.get('/pokemon', (req, res) => {
 res.render('index.ejs', { data: pokemon });
 });
 
+//DESTROY
+app.delete('/pokemon/:indexOfPokemonArray', (req, res) =>{
+    pokemon.splice(req.params.indexOfPokemonArray,1)
+    res.redirect('/pokemon')
+})
+
+  //EDIT 
+app.get("/pokemon/:indexOfPokemonArray/edit", (req, res) => {
+    res.render("edit.ejs", {
+        pokedex: pokemon[req.params.indexOfPokemonArray],
+        index: req.params.indexOfPokemonArray
+    })
+  })
+  
+  //UPDATE
+app.put('/pokemon/:indexOfPokemonArray', (req, res)=>{
+    pokemon[req.params.indexOfPokemonArray] = req.body
+    res.redirect('/pokemon')
+  })
+
+
 //NEW
-app.get('/new', (req, res)=>{
+app.get('/pokemon/new', (req, res)=>{
     res.render('new.ejs')
 })
 
-app.post('/', (req, res)=>{    
+app.post('/pokemon', (req, res)=>{    
     pokemon.push(req.body)
-    res.redirect('/')
+    res.redirect('/pokemon')
 })
-
-//DESTROY
-app.delete('/:indexOfPokemonArray', (req, res) =>{
-    pokemon.splice(req.params.indexOfPokemonArray,1)
-    res.redirect('/')
-})
-
 
 //SHOW 
-app.get('/:id', (req, res) => {
+app.get('/pokemon/:id', (req, res) => {
 res.render('show.ejs', { poke: pokemon[req.params.id] });
 });
 
